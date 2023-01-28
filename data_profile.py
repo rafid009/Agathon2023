ba187d2979a6c8209d95572bc2ff90c35fd8b1f8
rafid009 <rfd.009@gmail.com>
2023-01-28 18:47:47 +0000

    data profile created as an HTML

--- data_profile.py
+++ data_profile.py
@@ -1,3 +1,24 @@
 import numpy as np
 import pandas as pd
+import os
+from pandas_profiling import ProfileReport
 
+
+filepath = "./00_all_ClimateIndices_and_precip.csv"
+
+df = pd.read_csv(filepath)
+
+features = df.columns.values
+
+print(features)
+print("\n\n")
+for col in features:
+    if col == "date":
+        continue
+    missing = df[col].isna().sum()
+    print(f"For Feature: {col} data rate of missing data: {(missing*100)/len(df)}")
+
+print(f"\n\n{df.describe()}")
+
+profile = ProfileReport(df, title="Pandas Profiling Report")
+profile.to_file("data_report.html")
