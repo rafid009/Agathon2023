68c63521a605f85eeda39c5be6179bef48894d4e
Brandon Admire <admireb@oregonstate.edu>
2023-01-29 05:37:17 +0000

    loss not lowering, needs wizard review

--- gru-lstm.py
+++ gru-lstm.py
@@ -7,33 +7,53 @@ from utils import get_time_series_data,get_mean_std,get_normalized_data
 from sklearn.model_selection import KFold
 
 class LSTM_model(nn.Module):
-    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
+    def __init__(self, input_size, hidden_size, seq_length,training=False):
         super(LSTM_model, self).__init__()
-        self.num_classes = num_classes #number of classes
-        self.num_layers = num_layers #number of layers
         self.input_size = input_size #input size
-        self.hidden_size = hidden_size #hidden state
+        self.hidden_size = hidden_size #number of hidden "nodes"
         self.seq_length = seq_length #sequence length
+        self.train_flag = training #Flag to indicate if model is training model, set to True to turn on feature forcing
 
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size,
-                          num_layers=num_layers, batch_first=True) #lstm
-        self.fc_1 =  nn.Linear(hidden_size, 128) #fully connected 1
-        self.fc = nn.Linear(128, num_classes) #fully connected last layer
+        self.cell = nn.LSTMCell(input_size=input_size, hidden_size=hidden_size)
+        self.fc_1 = nn.Linear(hidden_size,seq_length) #fully connected 1
+        self.fc = nn.Linear(seq_length,1) #fully connected last layer
 
         self.relu = nn.ReLU()
 
-    def forward(self,x):
-        h_0 = Variable(pt.zeros(self.num_layers, x.size(0), self.hidden_size)) #hidden state
-        c_0 = Variable(pt.zeros(self.num_layers, x.size(0), self.hidden_size)) #internal state
-        # Propagate input through LSTM
-        output, (hn, cn) = self.lstm(x, (h_0, c_0)) #lstm with input, hidden, and internal state
-        print("hn: ", hn)
-        hn = hn.view(-1, self.hidden_size) #reshaping the data for Dense layer next
-        out = self.relu(hn)
-        out = self.fc_1(out) #first Dense
-        out = self.relu(out) #relu
-        out = self.fc(out) #Final Output
-        return out
+    def forward(self,x,y):
+        h_0 = Variable(pt.zeros(self.seq_length,self.hidden_size)) #hidden state
+        c_0 = Variable(pt.zeros(self.seq_length,self.hidden_size)) #internal state
+
+        # print("h_0: ",h_0.shape)
+        # print("c_0: ",c_0.shape)
+
+        x_loss = 0.0
+
+        for t in range(x.size(0)):
+            cur_x = x[t]
+            # print("Inner loop x shape:",cur_x.shape)
+            hn,cn = self.cell(cur_x, (h_0, c_0)) #lstm with input, hidden, and internal state
+            # print("hn: ",hn.shape)
+            # print("cn: ",cn.shape)
+
+            # Linear layer
+            yp = hn.view(-1, self.hidden_size) #reshaping the data for Dense layer next
+            yp = self.relu(yp)
+            yp = self.fc_1(yp) #first Dense
+            yp = self.relu(yp) #relu
+            yp = self.fc(yp) #Final Output
+            
+            # Calculate loss
+            x_loss += pt.sum(pt.pow(y[t] - yp,2)) / yp.shape[0] + 1e-5
+
+            h_0 = hn
+
+        return yp,x_loss
+
+# Take loss from each layer, then final loss is average of all losses.
+# LSTMCell, manually 
+
+# If training do the feature forcing
 
 if __name__ == "__main__":
     # Set random seed
@@ -41,11 +61,12 @@ if __name__ == "__main__":
 
     # Hyper parameters
     num_epochs = 1000
-    learning_rate = 0.001
 
     # Load, process, and split raw data
     raw_data = pd.read_csv("data/00_all_ClimateIndices_and_precip.csv",na_values=0)
     X,Y = get_time_series_data(raw_data)
+    # Set sequence length given from get_time_series_data
+    seq_length = X.shape[1]
 
     print(f"X shape: ",X.shape)
     print("(Year, month, feature)")
@@ -57,68 +78,38 @@ if __name__ == "__main__":
 
     # Derrive meta statistics
     feature_num = X_norm.shape[2] #calculated on data
-    LAYERS = 1 # Number of layers to run, then blend
-    hidden_size = feature_num * LAYERS
+    hidden_size = seq_length
     FOLDS = 5
-
-    # Define loss function
-    loss = nn.MSELoss()
 
     # Define K-fold utility
     kfold = KFold(n_splits=FOLDS)
 
-    # initialize hidden container
-    hidden = ((pt.randn(1,7,hidden_size)),(pt.randn(1,7,hidden_size)))
-
     # Start training
-    print("---Starting Job---")
-    for fold, (train_ids, test_ids) in enumerate(kfold.split(X_norm)):
-        print(f"Starting fold # {fold}")
 
-        # Split out training data
-        cur_X_train = X_norm[train_ids]
-        cur_Y_train = Y[train_ids]
-
-        # Split out test data
-        cur_X_test = X_norm[test_ids]
-        cur_Y_test = Y[test_ids]
-
-        print("Training Shape: ",cur_X_train.shape)
-        print("Testing Shape: ",cur_X_test.shape)
-        
-        # Create tensors for later
-        X_train_tensors = Variable(pt.Tensor(cur_X_train))
-        X_test_tensors = Variable(pt.Tensor(cur_X_test))
-
-        Y_train_tensors = Variable(pt.Tensor(cur_Y_train))
-        Y_test_tensors = Variable(pt.Tensor(cur_Y_test))
+    print("Training Shape: ",X_norm.shape)
     
-        # print(X_train_tensors.shape,Y_train_tensors.shape)
-        # print(X_test_tensors.shape,Y_test_tensors.shape)
-        # Initialize model
-        model = LSTM_model(11,feature_num,hidden_size,LAYERS,X_train_tensors.shape[1])
-        optimizer = pt.optim.Adam(model.parameters(), lr=learning_rate) 
+    # Create tensors for later
+    X_tensors = Variable(pt.Tensor(X_norm))
+    Y_tensors = Variable(pt.Tensor(Y))
 
-        for epoch in range(num_epochs):
-            outputs = model.forward(X_train_tensors)
-            optimizer.zero_grad()
-            
-            # print("inputs for loss function: ")
-            # print("outputs: ",outputs)
-            # print("Y_train_tensors: ",Y_train_tensors)
+    X_tensors = pt.nan_to_num(X_tensors)
+    Y_tensors = pt.nan_to_num(Y_tensors)
 
-            cur_loss = loss(outputs,Y_train_tensors)
+    # Initialize model
+    model = LSTM_model(feature_num,hidden_size,seq_length)
 
-            cur_loss.backward()
+    for epoch in range(num_epochs):
 
-            optimizer.step()
+        outputs,loss = model.forward(X_tensors,Y_tensors)
 
-            if epoch % 100 == 0:
-                print("Epoch: %d, loss: %1.5f" % (epoch, cur_loss.item()))
+        loss.backward()
 
-        # out,hidden = lstm(pt.from_numpy(X_norm),hidden)
-        # print("out: ",out)
-        # print("hidden: ",hidden)
+        if epoch % 100 == 0:
+            print("Epoch: %d, loss: %1.5f" % (epoch, loss))
+
+    # out,hidden = lstm(pt.from_numpy(X_norm),hidden)
+    # print("out: ",out)
+    # print("hidden: ",hidden)
 
     # for i,year in enumerate(X_norm):
     #     print(year.shape)
